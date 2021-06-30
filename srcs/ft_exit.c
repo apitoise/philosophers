69bbaf7d8f8b396a1d470a5df954705132e9daf5
apitoise <arthur.pitoiset@outlook.fr>
2021-06-30 14:36:23 +0200

    get normed

--- srcs/ft_exit.c
+++ srcs/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: apitoise <apitoise@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 16:53:51 by apitoise          #+#    #+#             */
-/*   Updated: 2021/06/29 17:29:03 by apitoise         ###   ########.fr       */
+/*   Updated: 2021/06/30 14:24:10 by apitoise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	ft_exit(t_struct *st, t_philo *philo)
 		i++;
 	}
 	free(st->forks);
+	free(st->state);
 	free(philo);
 	pthread_mutex_destroy(&st->print);
 }
